e50534caa3abb45e8c77e92513a525e044482d32
Ameerhamxxa <14beseanasir@seecs.edu.pk>
2017-02-22 12:51:33 +0500

    lab2
    matrix multiplication

---
+++ Matrix_Multiply/Matrix_Multiply/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Multiply
+{
+    public class Class1
+    {
+        //a FUNCTION which recieves two square matrices and row and column numbers
+        public int[,] Multiply (int[,] a,int[,] b, int row,int column)
+        {
+            //new multidimensional matrix for the result
+            int[,] result = new int[row, column];
+            //first for loop for rows
+            for (int i=0;i<row;i++)
+            {
+                //second for loop for traversing through columns
+                for(int j=0;j<column;j++)
+                {
+                    //initializing result matrix with 0
+                    result[i,j] = 0;
+                    //third for loop for calculating results
+                    for(int k=0; k<row; k++)
+                    {
+                        result[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            //returning result
+            return result;
+        }
+    }
+}
---
+++ Matrix_Multiply/Matrix_MultiplyTests/Class1Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matrix_Multiply;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Multiply.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            //hardcode data
+            int row = 2;
+            int column = 2;
+            int[,] arr1 = { { 1, 2 },{ 3, 4 } };
+            int[,] arr2 = { { 2, 4 }, { 5, 3 } };
+            //creating object of class
+            Class1 obj1 = new Class1();
+            //expected output
+            int[,] answer = { { 12, 10 }, { 26, 24 } };
+            //calling library
+            int [,] result=obj1.Multiply(arr1,arr2,row,column);
+            //for loop for comparison
+            for(int i=0;i<row;i++)
+            {
+                for(int j=0;j<column;j++)
+                {
+                    Assert.AreEqual(result[i,j], answer[i,j]);
+                }
+            }
+            
+        }
+    }
+}
